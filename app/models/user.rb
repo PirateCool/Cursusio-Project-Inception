399b770fc3e783a6bfd27d8050f880e54a4415ba
Runefire32 <thomas_g@orange.fr>
2018-11-01 19:17:03 +0100

    woaw

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable,
+         :validatable, authentication_keys: [:login]
 
         # Généré avec 'rails g devise' (template)
         # Devise - Etape 1 : accepté Username en tant que parametre de registration
@@ -11,4 +11,40 @@ class User < ApplicationRecord
 
         has_many :courses
         has_many :discussions
+
+
+       
+
+        attr_writer :login
+ 	validate :validate_username
+
+	def validate_username
+  		if User.where(email: username).exists?
+    		errors.add(:username, :invalid)
+  		end
+	end
+
+     #case sensitive
+ 	def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
+    end
+  end
+
+    
+	end
+
+
+  def login
+    @login || self.username || self.email
+  end
+
+
+
 end
